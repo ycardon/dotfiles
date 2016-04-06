be640b1a308bba3129200d40ec43f6a3197a46af
Yann Cardon <yann.cardon@orange.com>
2016-04-06 09:53:40 +0200

    fish config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+
+# Prompt config (used by bobthefish theme)
+set -g theme_date_format "+%a %H:%M"
+
+# Fisherman init
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
