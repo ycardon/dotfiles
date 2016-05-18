a4de2c9f75f7f279ee138112f1946ad58fc66b80
Yann Cardon <yann.cardon@orange.com>
2016-05-18 10:36:44 +0200

    (fish) new fisherman plugins

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,10 @@
 set -x EDITOR vim
 set -x TERM xterm-256color
 
+# go
+set -x GOPATH /home/ycardon/go
+set -x PATH $PATH $GOPATH/bin
+
 # Prompt config (used by bobthefish theme)
 set -g theme_date_format "+%a %H:%M"
 set -g theme_nerd_fonts yes
