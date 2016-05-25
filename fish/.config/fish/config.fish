ef1d689e361882b16b70ab44756b194275de5ad7
Yann Cardon <yann.cardon@orange.com>
2016-05-25 20:15:30 +0200

    (fish) default pager command

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,14 +2,11 @@
 # common env variables
 set -x EDITOR vim
 set -x TERM xterm-256color
+set PAGER 'less -r -E -X'
 
 # go
 set -x GOPATH /home/ycardon/go
 set -x PATH $PATH $GOPATH/bin
-
-# nvm
-#set -gx NVM_DIR /usr/share/nvm
-
 
 # Prompt config (used by bobthefish theme)
 set -g theme_date_format "+%a %H:%M"
